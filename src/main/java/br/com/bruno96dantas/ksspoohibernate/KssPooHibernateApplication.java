07f8fce50d7e840bd9c57b7a8987e670aa539316
Bruno Dantas <bruno96dantas@gmail.com>
2019-03-29 10:54:14 -0300

    init commit

---
+++ src/main/java/br/com/bruno96dantas/ksspoohibernate/KssPooHibernateApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno96dantas.ksspoohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KssPooHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KssPooHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bruno96dantas/ksspoohibernate/KssPooHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.bruno96dantas.ksspoohibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KssPooHibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
